82dffbb43fe603b195430c3685403d4a906c7735
Malihanaz <149990526+Malihanaz@users.noreply.github.com>
2023-12-28 23:05:15 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,26 @@
+create database SchoolDb
+
+Create table Student
+(
+StudentId int primary key,
+FirstName nvarchar(50),
+LastName nvarchar(50),
+ClassName int foreign key references Classes(ClassId) on delete cascade on update cascade,
+SubjectName int foreign key references Subjects(SubjectId) on delete cascade on update cascade 
+)
+
+create table StudentSubjects
+(
+SubjectId int primary key,
+SubjectName nvarchar(50)
+)
+
+create table studentclass
+(
+ClassId int primary key,
+ClassName nvarchar(50)
+)
+
+select * from Student
+select * from StudentSubjects
+select * from Studentclass
